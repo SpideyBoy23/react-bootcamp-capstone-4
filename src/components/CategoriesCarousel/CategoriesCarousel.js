3c394a266095b75726a6b6dd498e77602942c79c
Charly Arreola <charly_cto@hotmail.com>
2022-03-07 11:46:39 -0600

    Added the category carousel

---
+++ src/components/CategoriesCarousel/CategoriesCarousel.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import {MdKeyboardArrowLeft, MdKeyboardArrowRight} from 'react-icons/md';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import './CategoriesCarousel.css';
+
+
+export const Categories = ({categories}) => {
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 3
+    };
+
+    return(
+        <>
+            <section className="CategoriesCarousel">
+                <div className="CategoriesCards">
+                    <Slider {...settings}>
+                        {categories.results.map((category, index) => {
+                                const { data: {name, main_image: { alt, url }} } = category
+                                return (
+                                    <div className="product-card" key={index}>
+                                        <img src={url} alt={alt} className="product-img" />
+                                        <div className="product-info">
+                                            <div>
+                                                <p>{name}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                )
+                            })}           
+                    </Slider>
+                </div>
+            </section>
+        </>
+    );
+}
