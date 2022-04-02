1b413e2e5ca04c3db34aa943e30666b5c89ec672
Charly Arreola <charly_cto@hotmail.com>
2022-04-02 00:23:11 -0600

    Refactored Featured Banners Slider using useFeaturedBanners Custom Hook

--- src/components/FeaturedSlider/FeaturedSlider.js
+++ src/components/FeaturedSlider/FeaturedSlider.js
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import {MdKeyboardArrowLeft, MdKeyboardArrowRight} from 'react-icons/md';
+
+import { useFeaturedBanners } from '../../utils/hooks/useFeaturedBanners';
+
 import './FeaturedSlider.css';
 
 
@@ -7,6 +10,7 @@ export const Slider = ({ slides }) => {
 
     const [currentSlide, setCurrentSlide] = useState(0);
     const length = slides.results.length;
+    const featuredBannersInfo = useFeaturedBanners();
 
     const nextSlide = () => {
         setCurrentSlide(currentSlide === length - 1 ? 0 : currentSlide + 1);
@@ -23,7 +27,7 @@ export const Slider = ({ slides }) => {
             <section className="slider">
                 <MdKeyboardArrowLeft className="Left-arrow"  onClick={previousSlide}/>
                 <MdKeyboardArrowRight className="Right-arrow" onClick={nextSlide}/>
-                {slides.results.map((slide, index) => {
+                {featuredBannersInfo.isLoading ? 'Loading' :  featuredBannersInfo.data.results.map((slide, index) => {
                     const { data: { main_image: {alt, url} } } = slide
                     return (
                         <div className={index == currentSlide ? 'slide active' : 'slide'} key={index}>
@@ -33,7 +37,6 @@ export const Slider = ({ slides }) => {
                         </div>
                         
                     )
-                
                 })}
             </section>
         </>
