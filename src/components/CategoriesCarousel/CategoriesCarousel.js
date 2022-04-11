da5fe271d36b8d93ca0c8bddf59ce6adb0dbc834
Charly Arreola <charly_cto@hotmail.com>
2022-04-11 00:10:01 -0500

    Added Link to selected category in Categories Carrousel

--- src/components/CategoriesCarousel/CategoriesCarousel.js
+++ src/components/CategoriesCarousel/CategoriesCarousel.js
@@ -1,11 +1,16 @@
 import Slider from "react-slick";
 import { Loader } from '../Loader/Loader';
+import { useContext }  from 'react';
+import { ShopContext } from '../../Context/context';
+import { Link } from "react-router-dom";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import './CategoriesCarousel.css';
 
 
 export const Categories = ({categories}) => {
+
+    const { setFilters } = useContext(ShopContext)
 
     const settings = {
         dots: false,
@@ -20,23 +25,25 @@ export const Categories = ({categories}) => {
             <section className="categories-carousel">
                 <div className="categories-cards">
                         {categories.isLoading 
-                        ? <Loader /> 
-                        : 
-                        <Slider {...settings}>
-                            {
-                            categories.data.results.map((category, index) => {
-                                const { data: {name, main_image: { alt, url }} } = category
-                                return (
-                                    <div className="product-card" key={index}>
-                                        <img src={url} alt={alt} className="product-img" />
-                                        <div className="product-info">
-                                            <div>
-                                                <p>{name}</p>
+                            ? <Loader /> 
+                            : 
+                            <Slider {...settings}>
+                                {
+                                categories.data.results.map((category, index) => {
+                                    const { id, data: {name, main_image: { alt, url }} } = category
+                                    return (
+                                        <Link to={'/products'} onClick={() => setFilters(id)}>
+                                            <div className="product-card" key={index} >
+                                                <img src={url} alt={alt} className="product-img" />
+                                                <div className="product-info">
+                                                    <div>
+                                                        <p>{name}</p>
+                                                    </div>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                )
-                            })}
+                                        </Link>
+                                    )
+                                })}
                             </Slider>
                         }           
                 </div>
